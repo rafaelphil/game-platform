f3aa676b127968b8b76e365cae66cd6c967a7ae6
Rafael <rafaelphilippou@gmail.com>
2026-07-06 20:48:09 +0300

    Define entity Library

---
+++ src/main/java/com/rafael/game_platform/library/Library.java
@@ -0,0 +1,34 @@
+package com.rafael.game_platform.library;
+
+import com.rafael.game_platform.games.Game;
+import com.rafael.game_platform.users.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"user_id","game_id"}))
+public class Library {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Min(0)
+    private Integer hoursPlayed;
+    private LocalDateTime lastPlayedAt;
+    private LocalDateTime addedAt;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+}
